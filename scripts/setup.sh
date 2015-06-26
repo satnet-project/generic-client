26216c4dc5b7b826f41af529a5bdd31925acb76a
Samuel Gongora Garcia <sgongar@debian8-64bits-dev>
2015-06-27 00:39:41 +0200

    Dependencies updated.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,6 +3,9 @@ script_path="$( cd "$( dirname "$0" )" && pwd )"
 project_path=$( readlink -e "$script_path/.." )
 venv_path="$project_path/.venv"
 
+# Install required packages
+sudo apt install python-qt4
+
 # Create a virtualenv
 virtualenv $venv_path
 source "$venv_path/bin/activate"

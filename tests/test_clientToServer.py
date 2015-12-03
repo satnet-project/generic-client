e40472b257a45140a91c0ae5e29671e9d008a482
sgongar <s.gongoragarcia@gmail.com>
2015-12-03 15:04:52 +0100

    Update

--- tests/test_clientToServer.py
+++ tests/test_clientToServer.py
@@ -4,18 +4,13 @@ import os
 import unittest
 import mock
 import time
-from mock import Mock, MagicMock
 
-from twisted.python import log
-from unittest import TestCase
+# from unittest import TestCase
 
 from twisted.internet import defer, protocol, reactor, ssl
 from twisted.internet.error import CannotListenError
-from twisted.internet.protocol import Factory
 from twisted.protocols.amp import AMP, Command, Integer, Boolean, String
 from twisted.python import log
-from twisted.trial.unittest import TestCase
-from twisted.test.proto_helpers import StringTransport
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from gs_interface import GroundStationInterface
@@ -44,7 +39,6 @@ __author__ = 's.gongoragarcia@gmail.com'
 
 
 class SendMsg(Command):
-   
     arguments = [('sMsg', String()),
                  ('iTimestamp', Integer())]
     response = [('bResult', Boolean())]
@@ -69,8 +63,7 @@ class TestClientToServer(unittest.TestCase):
 
     def mock_processframe(self, frame):
         CONNECTION_INFO = {}
-        gsi = object
-        
+        gsi = object       
         clientprotocol = ClientProtocol(CONNECTION_INFO, gsi)
         # Mock callremote
         clientprotocol.callRemote = mock.MagicMock(side_effect=self.mock_callremote)
@@ -81,15 +74,15 @@ class TestClientToServer(unittest.TestCase):
 
     def setUp(self):
         log.startLogging(sys.stdout)
-        log.msg(">>>>>>>>>>>>>>>>>>>>>>>>> Running tests")
+        log.msg(">>>>>>>>>>>>>>>>>>>>> Running tests")
         log.msg("")
 
         self.serverDisconnected = defer.Deferred()
         self.serverPort = self._listenServer(self.serverDisconnected)
         self.connected = defer.Deferred()
         self.clientDisconnected = defer.Deferred()
-        self.clientConnection = self._connectClient(self.connected,\
-         self.clientDisconnected)
+        self.clientConnection = self._connectClient(self.connected,
+                                                    self.clientDisconnected)
         return self.connected
 
     def _listenServer(self, d):
@@ -99,7 +92,7 @@ class TestClientToServer(unittest.TestCase):
 
             self.pf = CredAMPServerFactory()
             self.pf.protocol = CredReceiver()
-            self.pf.protocol.login = MagicMock(side_effect=self.mockLoginMethod)
+            self.pf.protocol.login = mock.MagicMock(side_effect=self.mockLoginMethod)
             self.pf.onConnectionLost = d
             cert = ssl.PrivateCertificate.loadPEM(
                 open('key/server.pem').read())
@@ -111,7 +104,6 @@ class TestClientToServer(unittest.TestCase):
         self.factory = protocol.ClientFactory.forProtocol(ClientProtocol)    
         self.factory.onConnectionMade = d1
         self.factory.onConnectionLost = d2
-
         cert = ssl.Certificate.loadPEM(open('key/public.pem').read())      
         options = ssl.optionsForClientTLS(u'example.humsat.org', cert)
         return reactor.connectSSL("localhost", 1234, self.factory, options)
@@ -120,18 +112,18 @@ class TestClientToServer(unittest.TestCase):
         try:
             d = defer.maybeDeferred(self.serverPort.stopListening)
             self.clientConnection.disconnect()
-            return defer.gatherResults([d, self.clientDisconnected,\
-             self.serverDisconnected])
+            return defer.gatherResults([d, self.clientDisconnected,
+                                        self.serverDisconnected])
         except AttributeError:
             self.clientConnection.disconnect()
-            return defer.gatherResults([self.clientDisconnected,\
-             self.serverDisconnected])            
+            return defer.gatherResults([self.clientDisconnected,
+                                        self.serverDisconnected])            
 
     """
     Send a correct frame without connection
     """
     def test_AMPnotPresentCorrectFrame(self):
-        log.msg(">>>>>>>>>>>>>>>>>>>>> Running AMPnotPresentCorrectFrame test")
+        log.msg(">>>>>>>>>>>>>>>>> Running AMPnotPresentCorrectFrame test")
 
         frame = 'Frame'
         CONNECTION_INFO = {}
@@ -141,17 +133,16 @@ class TestClientToServer(unittest.TestCase):
         gsi = GroundStationInterface(CONNECTION_INFO, GS, self.amp)
         gsi._manageFrame(frame)
 
-        assert os.path.exists("ESEO-" + GS + "-" +\
-         time.strftime("%Y.%m.%d") + ".csv") == 1
-        log.msg(">>>>>>>>>>>>>>>>>>>>> AMP not present - Local file created")
-        log.msg(">>>>>>>>>>>>>>>>>>>>> AMPnotPresentCorrectFrame test OK")
+        assert os.path.exists("ESEO-" + GS + "-" +
+                                time.strftime("%Y.%m.%d") + ".csv") == 1
+        log.msg(">>>>>>>>>>>>>>>>> AMP not present - Local file created")
+        log.msg(">>>>>>>>>>>>>>>>> AMPnotPresentCorrectFrame test OK")
 
     """
     Send an incorrect frame without connection
     """
     def _test_AMPnotPresentIncorrectFrame(self):
-
-        log.msg(">>>>>>>>>>>>>>>>>>>>> Running AMPnotPresentIncorrectFrame test")
+        log.msg(">>>>>>>>>>>>>>>>> Running AMPnotPresentIncorrectFrame test")
 
         frame = 1234
         CONNECTION_INFO = {}
@@ -160,17 +151,16 @@ class TestClientToServer(unittest.TestCase):
 
         gsi = GroundStationInterface(CONNECTION_INFO, GS, self.amp)
 
-        self.assertRaisesRegexp(WrongFormatNotification, "Bad format frame",\
-          lambda: gsi._manageFrame(frame))
-        log.msg(">>>>>>>>>>>>>>>>>>>>> AMP not present - Local file not created")
-        log.msg(">>>>>>>>>>>>>>>>>>>>> AMPnotPresentIncorrectFrame test OK")
+        self.assertRaisesRegexp(WrongFormatNotification, "Bad format frame",
+                                lambda: gsi._manageFrame(frame))
+        log.msg(">>>>>>>>>>>>>>>>> AMP not present - Local file not created")
+        log.msg(">>>>>>>>>>>>>>>>> AMPnotPresentIncorrectFrame test OK")
 
     """
     Send an incorrect frame with connection
     """
     def _test_AMPPresentIncorrectFrame(self):
-
-        log.msg(">>>>>>>>>>>>>>>>>>>>> Running AMPPresentIncorrectFrame")
+        log.msg(">>>>>>>>>>>>>>>>>>Running AMPPresentIncorrectFrame")
 
         frame = 1234
         CONNECTION_INFO = {}
@@ -179,24 +169,26 @@ class TestClientToServer(unittest.TestCase):
 
         gsi = GroundStationInterface(CONNECTION_INFO, GS, self.amp)
 
-        self.assertRaisesRegexp(Exception, "Bad format frame",\
-          lambda: gsi._manageFrame(frame))
-        log.msg(">>>>>>>>>>>>>>>>>>>>> Error - Local file not created")
-        log.msg(">>>>>>>>>>>>>>>>>>>>> AMPPresentIncorrectFrame test OK")
+        self.assertRaisesRegexp(Exception, "Bad format frame",
+                                lambda: gsi._manageFrame(frame))
+        log.msg(">>>>>>>>>>>>>>>>> Error - Local file not created")
+        log.msg(">>>>>>>>>>>>>>>>> AMPPresentIncorrectFrame test OK")
 
     """
     Send a correct frame with connection
     """
     def _test_AMPPresentCorrectFrame(self):
-        log.msg(">>>>>>>>>>>>>>>>>>>>> Running AMPpresentCorrectFrame test")
+        log.msg(">>>>>>>>>>>>>>>>> Running AMPpresentCorrectFrame test")
 
         frame = 'Frame'
         CONNECTION_INFO = {}
         GS = 'Vigo'
 
         self.amp = AMP()
-        self.amp._processframe = mock.MagicMock(side_effect=self.mock_processframe)
-        gsi = GroundStationInterface(CONNECTION_INFO, GS, self.amp)._manageFrame(frame)
+        self.amp._processframe = mock.MagicMock(
+            side_effect=self.mock_processframe)
+        gsi = GroundStationInterface(
+            CONNECTION_INFO, GS, self.amp)._manageFrame(frame)
 
 
 if __name__ == '__main__':
